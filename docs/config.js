38b2e41c6e6040a91e887913f70ebf3302a2a34c
Buvani Pai <bhuvaniprakashpai@gmail.com>
2026-04-21 19:32:52 -0500

    point frontend at EC2 API

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 // config.js
 const API_BASE_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
   ? "http://localhost:8000"
-  : "https://freightpilot-api-t75n.onrender.com";
+  : "http://18.188.8.150:8080";
