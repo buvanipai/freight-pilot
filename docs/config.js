22a9f545d4efb3976f60d6d2fe96740dfa163c25
Buvani Pai <bhuvaniprakashpai@gmail.com>
2026-05-28 00:48:12 -0500

    fix: point frontend at new EC2 IP 18.116.51.0

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 // config.js
 const API_BASE_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
   ? "http://localhost:8000"
-  : "http://18.188.8.150:8080";
+  : "http://18.116.51.0";
