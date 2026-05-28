596bcbbd8ec0c5be24c017fe54ef540a94ea09d8
Buvani Pai <bhuvaniprakashpai@gmail.com>
2026-05-28 18:46:33 -0500

    fix: point frontend at CloudFront HTTPS endpoint

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 // config.js
 const API_BASE_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
   ? "http://localhost:8000"
-  : "http://18.116.51.0";
+  : "https://d2yu3oa51qymd0.cloudfront.net";
